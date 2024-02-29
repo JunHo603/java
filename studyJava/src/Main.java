2c1f3202048f5d819ac6476419e6ca630f99baf7
JunHo603 <63783891+JunHo603@users.noreply.github.com>
2024-02-29 17:25:53 +0900

    test

--- studyJava/src/Main.java
+++ studyJava/src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
-        //System.out.println("Hello world!");
+        //int a = 5;
+        //int b = 3;
+        int x = 61;
+        System.out.println(x>=60 ? "합격":"탈락");
         //int i = 9;
-        for(int i=0; i<10; i++){
+        /*for(int i=0; i<10; i++){
             if(i < 5){
                 System.out.print(i);
             } else{
                 System.out.println("숫자 끝");
                 break;
             }
-        }
+        }*/
     }
 }
